a8892282bf0f058bac5e5255c4f55ae14f262471
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-07-15 22:01:12 +0530

    Clean env

--- run_before_running_any_command.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
-source env/bin/activate
