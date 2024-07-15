7e9567093085d66659874f053b1dcec76a0bdc03
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-07-15 21:35:42 +0530

    Remove loop to calculate error from prompt

--- src/llm.py
+++ src/llm.py
@@ -1,35 +1,47 @@
 from llama_cpp import Llama
-import json
 
 err = 0
 llm = Llama(
     model_path="data/models/llama-2-7b.Q4_K_M.gguf",
     n_gpu_layers=-1,  # Uncomment to use GPU acceleration
-    verbose=False,
+    verbose=True,
     flash_attn=True,
     # seed=1337, # Uncomment to set a specific seed
     # n_ctx=2048, # Uncomment to increase the context window
 )
-for i in range(50):
-    output = llm.create_chat_completion(
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a classsifier that classify if user want to go somewhere(navigation intent) or not, you always outputs in JSON.",
-            },
-            {"role": "user", "content": "How to reach ballygunge from liluah"},
-        ],
-        response_format={
-            "type": "json_object",
-            "schema": {
-                "type": "object",
-                "properties": {"navigation_intent": {"type": "boolean"}},
-                "required": ["navigation_intent"],
-            },
-        },
-        temperature=0.7,
-    )
-    if "false" in output["choices"][0]["message"]["content"]:
-        err = err + 1
+output = llm.create_chat_completion(
+    messages=[
+        {
+            "role": "system",
+            "content": """You are a diligent and honest data entry expert, fluent in English and JSON.
+                        Your task is to parse user intent and required location out of text snippets.
 
-print(err)
+                        Rules:
+                        - leave unknown fields blank
+
+                        Respond with no other text but a JSON document like this example JSON document:
+                        {
+                        "intent": "",
+                        "locations": "",
+                        "to": in case of navigation intent,
+                        "from": in case of navigation intent,
+                        "answer": Reply as a helpful assistant
+                        }
+                        """,
+        },
+        {
+            "role": "user",
+            "content": "Input Data: I am new to kolkata, what are the good tourist spots",
+        },
+    ],
+    # response_format={
+    #     "type": "json_object",
+    #     "schema": {
+    #         "type": "object",
+    #         "properties": {"navigation_intent": {"type": "boolean"}},
+    #         "required": ["navigation_intent"],
+    #     },
+    # },
+    temperature=0.7,
+)
+print(output)
