1eab330a1881cb49421950781b91c5b248c4492a
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-07-17 21:56:27 +0530

    Fix bug where multiple files cant be generated on one tts_controller instance

--- src/tts_controller.py
+++ src/tts_controller.py
@@ -8,23 +8,18 @@ class TTS_controller:
         self.voice = PiperVoice.load(
             "/home/sujal/repos/untitled-ai-project/models/en_US-lessac-medium.onnx",
             "/home/sujal/repos/untitled-ai-project/models/en_US-lessac-medium.onnx.json",
-            use_cuda=False,
         )
 
     def generate(self, text, output_path):
         wav_file = wave.open(output_path, "w")
-        audio = self.voice.synthesize(text, wav_file)
+        self.voice.synthesize(text, wav_file)
 
 
 if __name__ == "__main__":
-    # TODO: Crashes if try to generate from same instance twice, creating a new instance take 1s extra
     start_time = time.time()
     tts = TTS_controller()
-
-    print(
-        tts.generate(
-            "Please generate a itenary for my kolkata trip  ",
-            "sample_audio/sample_command.wav",
-        )
+    tts.generate(
+        "Please generate a itenary for my kolkata trip  ",
+        "sample_audio/sample_command.wav",
     )
     print("--- %s seconds ---" % (time.time() - start_time))
