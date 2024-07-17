4d9ff6b675c9251d3f58eca2c8a39ccbac8a39ee
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-07-17 20:35:02 +0530

    Misc

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@ python3 -m venv venv
 source .venv/bin/activate
 pip install -r requirement.txt
 wget https://huggingface.co/QuantFactory/Meta-Llama-3-8B-Instruct-GGUF/resolve/main/Meta-Llama-3-8B-Instruct.Q4_K_M.gguf -P ./models
+wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json -
+P ./models
+wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/medium/en_US-lessac-medium.onnx -P ./m
+odels
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
