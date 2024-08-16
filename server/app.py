3bb39e036f79361b159808cf9011555cd198fc98
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-08-16 23:24:15 +0530

    Return answer instead on voice for on device tts

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from utils.main import Model_handler
 import os, time
 
@@ -12,12 +12,14 @@ def index():
     if request.method == "POST":
         f = request.files["file"]
         f.save(os.path.join("./server/static/received", f.filename))
-        model.generate(
+        answer, file_path = model.generate(
             input_file=f"server/static/received/{f.filename}",
             output_dir="server/static/generated/",
         )
-        time.sleep(2)
-        return "successful"
+        # while not os.path.exists(file_path):
+        #     time.sleep(0.1)
+        print(answer)
+        return answer
     else:
         return render_template("index.html")
 
